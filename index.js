25e197d774282c2e700b09b10db53268f7248dd7
Aditi Ohri <didiohri@gmail.com>
2019-07-26 00:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ function johnLennonFacts(facts) {
   const shoutFacts = [];
   let i = 0;
   while (i < facts.length) {
-    shoutFacts.push(`${facts[i]}!!!`)
+    shoutFacts.push(`${facts[i]}!!!`);
     i++;
   }
   return shoutFacts
+}
+
+function iLoveTheBeatles(n) {
+  const love = [];
+  do {
+    love.push(`I love the Beatles!`);
+    n++;
+  }
+  while (n < 15);
+  return love
 }
